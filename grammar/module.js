ce325f13e5c270cad7a91ba49c7a9403eea40200
Torsten Schmits <git@tryp.io>
2024-03-24 17:43:57 +0100

    Snapshot rewrite #2

---
+++ grammar/module.js
@@ -0,0 +1,140 @@
+const {
+  sep1,
+  sep,
+  parens,
+  semi,
+  semi_opt,
+  semis,
+} = require('./util.js')
+
+module.exports = {
+
+  // ------------------------------------------------------------------------
+  // module names
+  // ------------------------------------------------------------------------
+
+  _modid: $ => alias($.name, $.module_id),
+
+  _modid_prefix: $ => prec('qualifying-module', seq($._modid, $._any_tight_dot)),
+
+  _qualifying_module: $ => repeat1($._modid_prefix),
+
+  module: $ => seq(repeat($._modid_prefix), $._modid),
+
+  // ------------------------------------------------------------------------
+  // import/export
+  // ------------------------------------------------------------------------
+
+  namespace: _ => choice('pattern', 'type'),
+
+  _child_type: $ => seq(field('namespace', 'type'), field('type', $._tyconids)),
+ 
+  _child: $ => choice(
+    alias($._child_type, $.associated_type),
+    $._qname,
+  ),
+
+  children: $ => parens($, sep(',', field('element', choice(alias('..', $.all_names), $._child)))),
+
+  _ie_entity: $ => seq(
+    optional(field('namespace', $.namespace)),
+    choice(
+      field('variable', $._varids),
+      field('type', $._tyconids),
+      field('operator', choice($._sym_prefix, $._pqsym)),
+    ),
+    optional(field('children', $.children)),
+  ),
+
+  // ------------------------------------------------------------------------
+  // import
+  // ------------------------------------------------------------------------
+
+  import_list: $ => parens(
+    $,
+    sep(',', field('name', alias($._ie_entity, $.import_name))),
+    optional(','),
+  ),
+
+  import: $ => seq(
+    'import',
+    optional('qualified'),
+    optional(field('package', alias($.string, $.import_package))),
+    field('module', $.module),
+    optional('qualified'),
+    optional(seq('as', field('alias', $.module))),
+    optional(seq(
+      optional('hiding'),
+      field('names', $.import_list),
+    )),
+  ),
+
+  // ------------------------------------------------------------------------
+  // export
+  // ------------------------------------------------------------------------
+
+  module_export: $ => seq('module', field('module', $.module)),
+
+  exports: $ => parens(
+    $,
+    optional(sep1(',', choice(field('export', alias($._ie_entity, $.export)), $.module_export))),
+    optional(','),
+  ),
+
+  // ------------------------------------------------------------------------
+  // module body / sections
+  // ------------------------------------------------------------------------
+
+  header: $ => seq(
+    'module',
+    field('module', $.module),
+    field('exports', optional($.exports)),
+    $._where,
+  ),
+
+  imports: $ => seq(semis($, field('import', $.import)), semi($)),
+
+  /**
+   * Using `semi` at the end instead of `semi_opt` increases parser size by a full megabyte!!
+   */
+  declarations: $ => seq(semis($, $.declaration), semi_opt($)),
+
+  _body: $ => seq(
+    choice($._cmd_layout_start, alias($._cmd_layout_start_explicit, '{')),
+    semi_opt($),
+    field('imports', optional($.imports)),
+    field('declarations', optional($.declarations)),
+    $._layout_end,
+  ),
+
+  _layout_end: $ => choice(
+    $._cond_layout_end,
+    alias($._cond_layout_end_explicit, '}'),
+  ),
+
+  /**
+   * This is a supertype.
+   */
+  declaration: $ => choice(
+    $.decl,
+    $.type_synomym,
+    $.kind_signature,
+    $.type_family,
+    $.type_instance,
+    $.role_annotation,
+    $.data_type,
+    $.newtype,
+    $.data_family,
+    $.data_instance,
+    $.class,
+    $.instance,
+    $.default_types,
+    $.deriving_instance,
+    $.pattern_synonym,
+    $.foreign_import,
+    $.foreign_export,
+    $.fixity,
+    $.top_splice,
+  ),
+
+}
