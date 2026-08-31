ef9351be0d63849850ac25d3e5ce997e35ee8488
Madeleine Sydney Ślaga <msyds@deertopia.net>
2026-08-31 04:29:41 -0600

    Quoted -XOverloadedLabels

--- grammar/lexeme.js
+++ grammar/lexeme.js
@@ -2,6 +2,7 @@ const {
   id_char,
   varid_start_char,
   conid_start_char,
+  string_char,
 } = require('./util.js')
 
 module.exports = {
@@ -13,7 +14,10 @@ module.exports = {
   name: _ => token(seq(conid_start_char, id_char, /#*/)),
 
   label: _ => token(
-    /#[\pL\p{Mn}\pN_']+/,
+    choice(
+      seq('#', repeat1(id_char)),
+      seq('#"', repeat(string_char), '"'),
+    )
   ),
 
   _carrow: _ => choice('=>', '⇒'),
